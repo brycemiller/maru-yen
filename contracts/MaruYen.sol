662aee5d4a56be2d646066b42bfe8159a521362d
Bryce Miller <bryce.a.miller@googlemail.com>
2022-01-17 22:01:12 +0100

    WIP: [MARU-1] Improved deploy script to deploy to different networks.  Bumped solidity version and moved initial amount to inside contract

--- contracts/MaruYen.sol
+++ contracts/MaruYen.sol
@@ -1,11 +1,11 @@
 // contracts/MaruYen.sol
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.2;
 
 import "..\node_modules\@openzeppelin\contracts\token\ERC20\ERC20.sol";
 
 contract MaruYen is ERC20 {
-    constructor(uint256 initialSupply) ERC20("MaruYen", "MARY") {
-        _mint(msg.sender, initialSupply);
+    constructor() ERC20("MaruYen", "MARY") {
+        _mint(msg.sender, 10000 * 10 ** decimals());
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,42 @@
 const { Connection, Keypair, LAMPORTS_PER_SOL } = require("@solana/web3.js");
 const { Contract, publicKeyToHex } = require("@solana/solidity");
 const { readFileSync } = require("fs");
+const yargs = require("yargs");
 
 const MARUYEN_ABI = JSON.parse(readFileSync("./build/MaruYen.abi", "utf8"));
 const BUNDLE_SO = readFileSync("./build/bundle.so");
 const SECRET_KEY = JSON.parse(readFileSync("./id.json"));
+const CONTRACT_NAME = "MaruYen";
 
-const connectToSolana = (networkURL) => {
+const connect = (networkURL) => {
     return new Connection(networkURL, "confirmed");
 };
 
 const connectToLocalnet = () => {
-    return connectToSolana("http://localhost:8899");
+    console.log("Connecting to Localnet");
+    return connect("http://localhost:8899");
 };
 
 const connectToDevnet = () => {
-    return connectToSolana("https://api.devnet.solana.com");
+    console.log("Connecting to Devnet");
+    return connect("https://api.devnet.solana.com");
 };
 
+const connectToTestnet = () => {
+    console.log("Connecting to Testnet");
+    return connect("https://api.testnet.solana.com");
+}
+
 const airdropSol = async (connection, payer) => {
-    while (true) {
-        await connection.requestAirdrop(payer.publicKey, 1 * LAMPORTS_PER_SOL);
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        if (await connection.getBalance(payer.publicKey)) break;
-    }
+    console.log("Requesting airdrop");
+    await connection.requestAirdrop(payer.publicKey, 1 * LAMPORTS_PER_SOL);
+    console.log("Getting balance");
+    const balance = await connection.getBalance(payer.publicKey);
+    console.log(`Airdropped 1 SOL, ${payer.publicKey} balance: ${balance}`);
 };
 
 const deployContract = async (params) => {
-    const { connection, program, storage, abi, bundle, payer } = params;
+    const { connection, name, program, storage, abi, bundle, payer } = params;
 
     const contract = new Contract(
         connection,
@@ -36,40 +45,57 @@ const deployContract = async (params) => {
         abi,
         payer
     );
-
+    console.log("Loading contract");
     await contract.load(program, bundle, payer);
-    await contract.deploy("MaruYen", ['10000 * 10 **18'], program, storage, 4096 * 8);
+    console.log(`Deploying ${name} to ${program.publicKey}`);
+    const result = await contract.deploy(name, [], program, storage, 4096 * 8);
+    console.log("Contract deployed");
+    console.log(result);
 
     return contract;
 };
 
-const main = async (abi, bundle, secret) => {
-    const deploymentParams = ((abi, bundle, secret) => {
-        const payer = Keypair.fromSecretKey(new Uint8Array(secret));
-
+const main = async (connectionFun, abi, bundle, secret, name) => {
+    const deploymentParams = ((connection, abi, bundle, secret, name) => {
         return ({
-            connection: connectToDevnet(),
+            connection: connection(),
+            name: name,
             program: Keypair.generate(),
             storage: Keypair.generate(),
             abi: abi,
             bundle: bundle,
-            payer: payer,
-            address: publicKeyToHex(payer.publicKey),
+            payer: Keypair.fromSecretKey(new Uint8Array(secret)),
         });
-    })(abi, bundle, secret);
+    })(connectionFun, abi, bundle, secret, name);
 
-    const { address, connection, payer } = deploymentParams;
+    const { connection, payer, program, storage } = deploymentParams;
+
+    console.log(`ProgramID: ${program.publicKey}`);
+    console.log(`StorageID: ${storage.publicKey}`);
 
     await airdropSol(connection, payer);
-
     const contract = await deployContract(deploymentParams);
-
+    console.log("Getting symbol");
     const symbol = await contract.symbol();
-    const balance = await contract.balanceOf(address);
+    console.log("Getting balance");
+    const balance = await connection.getBalance(payer.publicKey);
+    const contractBalance = await contract.balanceOf(publicKeyToHex(payer.publicKey));
 
     console.log(`ERC20 contract for ${symbol} deployed!`);
-    console.log(`Wallet at ${address} has a balance of ${balance}${symbol}.`);
-
+    console.log(`Wallet at ${payer.publicKey} has a balance of ${contractBalance}${symbol}.`);
+    console.log(`Wallet at ${payer.publicKey} has a balance of ${balance}${symbol}.`);
 };
 
-main(MARUYEN_ABI, BUNDLE_SO, SECRET_KEY);
+let network = connectToLocalnet;
+switch(yargs.argv.network) {
+    case "dev":
+        network = connectToDevnet;
+        break;
+    case "test":
+        network = connectToTestnet;
+        break;
+    default:
+        break;
+}
+
+main(network, MARUYEN_ABI, BUNDLE_SO, SECRET_KEY, CONTRACT_NAME);
